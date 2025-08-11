56c3eeb4959c8abc313c6e19136fef88a14e157b
Cellyane Lima <cellyane@gmail.com>
2025-08-10 23:07:23 -0300

    Add top image

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,7 +1,16 @@
 function Footer() {
   return (
     <footer className="footer">
-      <p>asdfghjkl</p>
+      <p>
+        Header image from{' '}
+        <a
+          href="https://www.freepik.com/free-vector/builders-construction-workers-helmets_27577124.htm#from_element=cross_selling__vector?log-in=google"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Freepik
+        </a>{' '}
+      </p>
     </footer>
   )
 }
